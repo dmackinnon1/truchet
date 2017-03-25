2fdb91d232e92cabd3b835f91bd847227c9f8a10
dmackinnon1 <dmackinnon1@gmail.com>
2017-03-25 14:18:28 -0400

    refactored functions, working on reflection

--- js/truchet.js
+++ js/truchet.js
@@ -1,124 +1,16 @@
 
 var truchet = {};
 truchet.tiles = null;
-truchet.reflect = false;
-truchet.rotate = true;
-truchet.style="traditional";
 truchet.border = false;
-truchet.translate = false;
 
 truchet.start = function(size, rows) {
-
 	truchet.tiles = new Tiles(size,rows);
 	truchet.tiles.init();
 };
 
-class Tiles {
-
-	constructor(size, rows) {
-		this.size = size;
-		this.rows = rows;
-		this.cols = rows;
-		this.tiles = [];
-	}
-
-	init() {
-		for (var i = 0; i < this.rows; i++){
-			this.tiles[i] = [];
-			for (var j = 0; j < this.cols; j ++) {
-				this.tiles[i].push(0); 
-			} 
-		}	
-	}
-
-	htmlTable() {
-		var html = new Bldr("table").att("align", "center"); 
-		for (var i = 0; i < this.rows; i++){
-			var row = new Bldr("tr");
-			for (var j = 0; j < this.cols; j ++) {
-				var cell = new Bldr("td");
-				cell.elem(this.newTruchet(i,j,this.tiles[i][j]));
-				row.elem(cell); 
-			} 
-			html.elem(row);
-		}
-		return html.build();	
-	}
-	
-	newTruchet(i,j,k) {
-		var frame = new Bldr("svg").att("id", this.count + "tile_"+i+"_"+j)
-					.att("data-row", i).att("data-col", j)
-					.att("align", "center").att("width", this.size).att("height", this.size);		
-		var tile = null;
-		if (truchet.style == "traditional") {
-			tile = this.tileFill(k);
-		} else if (truchet.style == "diagonal") {
-			tile = this.tileDiag(k);
-		} else {
-			tile = this.tileSmith(k);
-		}
-		if (truchet.border) {
-			var border = new Bldr("rect").att("stroke-width",1).att("fill","none");
-			border.att("height", this.size).att("width", this.size).att("stroke","grey")
-				.att("x", 0).att("y",0);
-			frame.elem(border);
-		}
-		tile.att("data-row", i).att("data-col", j);
-		frame.elem(tile);
-		frame.att("onclick","elementClick(event)");
-		return frame;
-	}
-
-	element(i,j) {
-		return $("#" + "tile_"+i+"_"+j);
-	}		
-
-	rotate(i,j) {
-		this.tiles[i][j] = (this.tiles[i][j] + 1) % 4;
-	}
-
-	applyRules(i,j){
-		if (truchet.reflect) {
-			var x = this.rows -1 -i;
-			var y = this.rows -1 -j;
-			this.tiles[i][y] = (this.tiles[i][j] + 3) % 4;
-			this.tiles[x][j] = (this.tiles[i][j] + 1) % 4;
-			this.tiles[x][y] = (this.tiles[i][j] +2) % 4;
-			return;
-			
-		}
-		if (truchet.rotate) {
-			var x = this.rows -1 -i;
-			var y = this.rows -1 -j;
-			this.tiles[j][x] = (this.tiles[i][j] + 1) % 4;
-			this.tiles[x][y] = (this.tiles[i][j] + 2) % 4;
-			this.tiles[y][i] = (this.tiles[i][j] + 3) % 4;
-			return;	
-		}
-		
-		if (truchet.translate) {
-			var m = Math.ceil(this.rows/2);
-			var x = this.rows -1 - (m-i);
-			var y = this.rows -1 - (m-j);
-			this.tiles[x][j] = (this.tiles[i][j]);
-			this.tiles[i][y] = (this.tiles[i][j]);
-			this.tiles[x][y] = (this.tiles[i][j]);
-			return;		
-		}
-			
-	}
-
-	applyAll() {
-		for(var i = 0; i <this.rows; i++){
-			for (var j = 0; j<this.rows; j++){
-				this.applyRules(i,j);
-			}
-		}
-	}
-
-	tileFill(rotation) {
+truchet.tileTraditional = function(rotation) {
 		var tile = new Bldr("polygon").att("stroke-width",0).att("fill","black");
-		var c = this.size;
+		var c = truchet.tiles.size;
 		var tl = "0,0";
 		var tr = c +",0";
 		var bl = "0," + c;
@@ -134,9 +26,9 @@ class Tiles {
 			tile.att("points", br + " " + bl + " " + tl);		
 		}
 		return tile;
-	}
+};
 
-	tileSmith(rotation) {
+truchet.tileSmith = function(rotation) {
 		var tile = new Bldr("path").att("style","stroke:rgb(0,0,255);stroke-width:3")
 			.att("stroke-linecap","square")
 			.att("fill","none");
@@ -146,7 +38,7 @@ class Tiles {
 			.att("stroke-linecap","square")
 			.att("fill","none");
 
-		var c = this.size;
+		var c = truchet.tiles.size;
 		if (rotation %2 == 0) {
 			var arc ="M " + c/2 + " 0";
 			arc += " A " + c/2 + " " + c/2 + " "; //radii
@@ -178,12 +70,13 @@ class Tiles {
 		}
 		var group = new Bldr("g").elem(tile).elem(tile1);
 		return group;
-	}
+};
 
-	tileDiag(rotation) {
+
+truchet.tileDiag = function(rotation) {
 		var tile = new Bldr("line").att("style","stroke:rgb(0,0,255);stroke-width:3")
 			.att("stroke-linecap","square");
-		var c = this.size;
+		var c = truchet.tiles.size;
 		if (rotation % 2 == 0) {
 			tile.att("x1", c);
 			tile.att("y1", 0);
@@ -196,6 +89,107 @@ class Tiles {
 			tile.att("y2",c);		
 		}
 		return tile;
+};
+
+truchet.tileStyle = truchet.tileTraditional;
+
+
+truchet.ruleNone = function(i,j) {
+	//do nothing
+};
+
+truchet.ruleRotate = function(i,j) {
+	var t = truchet.tiles;
+	var x = t.rows -1 -i;
+	var y = t.rows -1 -j;
+	t.tiles[j][x] = (t.tiles[i][j] + 1) % 4;
+	t.tiles[x][y] = (t.tiles[i][j] + 2) % 4;
+	t.tiles[y][i] = (t.tiles[i][j] + 3) % 4;
+	return;	
+};
+
+truchet.ruleReflect = function(i,j) {
+	var t = truchet.tiles;
+	var x = t.rows -1 -i;
+	var y = t.rows -1 -j;
+	t.tiles[i][y] = (t.tiles[i][j] + 3) % 4;
+	t.tiles[x][j] = (t.tiles[i][j] + 1) % 4;
+	t.tiles[x][y] = (t.tiles[i][j] +2) % 4;
+	return;	
+};
+
+truchet.rule = truchet.ruleRotate;
+
+class Tiles {
+
+	constructor(size, rows) {
+		this.size = size;
+		this.rows = rows;
+		this.cols = rows;
+		this.tiles = [];
+	}
+
+	init() {
+		for (var i = 0; i < this.rows; i++){
+			this.tiles[i] = [];
+			for (var j = 0; j < this.cols; j ++) {
+				this.tiles[i].push(0); 
+			} 
+		}	
+	}
+	
+	htmlTable() {
+		var html = new Bldr("table").att("align", "center"); 
+		for (var i = 0; i < this.rows; i++){
+			var row = new Bldr("tr");
+			for (var j = 0; j < this.cols; j ++) {
+				var cell = new Bldr("td");
+				cell.elem(this.newTruchet(i,j,this.tiles[i][j]));
+				row.elem(cell); 
+			} 
+			html.elem(row);
+		}
+		return html.build();	
+	}
+	
+	newTruchet(i,j,k) {
+		var frame = new Bldr("svg").att("id", this.count + "tile_"+i+"_"+j)
+					.att("data-row", i).att("data-col", j)
+					.att("align", "center").att("width", this.size).att("height", this.size);		
+		
+		var tile = truchet.tileStyle(k);
+		
+		if (truchet.border) {
+			var border = new Bldr("rect").att("stroke-width",1).att("fill","none");
+			border.att("height", this.size).att("width", this.size).att("stroke","grey")
+				.att("x", 0).att("y",0);
+			frame.elem(border);
+		}
+		tile.att("data-row", i).att("data-col", j);
+		frame.elem(tile);
+		frame.att("onclick","elementClick(event)");
+		return frame;
+	}
+
+	element(i,j) {
+		return $("#" + "tile_"+i+"_"+j);
+	}		
+
+	rotate(i,j) {
+		this.tiles[i][j] = (this.tiles[i][j] + 1) % 4;
+	}
+
+	applyRules(i,j){
+		truchet.rule(i,j);
+			
+	}
+
+	applyAll() {
+		for(var i = 0; i <this.rows; i++){
+			for (var j = 0; j<this.rows; j++){
+				this.applyRules(i,j);
+			}
+		}
 	}
 
 	randomizeTile() {
